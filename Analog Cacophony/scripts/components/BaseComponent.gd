694d8922172d7c27ed4e58371d2b8fae42a8b875
winterbeak <winterbeak@gmail.com>
2022-07-02 10:42:28 -0400

    Store key in light

--- Analog Cacophony/scripts/components/BaseComponent.gd
+++ Analog Cacophony/scripts/components/BaseComponent.gd
@@ -37,10 +37,8 @@ func fail() -> void:
 func on_key_press(key: String) -> void:
 	pass
 
-# Detects input events, and passes the typed key into on_key_press.
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.is_pressed() and not event.is_echo():
-		var typed_event = event as InputEventKey
-		var key_typed = PoolByteArray([typed_event.unicode]).get_string_from_utf8()
-		if (key_typed in VALID_KEYS) and activated:
-			on_key_press(key_typed)
+func _process(delta: float) -> void:
+	if activated:
+		for key in VALID_KEYS:
+			if Input.is_action_just_pressed(key):
+				on_key_press(key)
--- Analog Cacophony/scripts/components/Light.gd
+++ Analog Cacophony/scripts/components/Light.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const RED_TIME: float = 0.8
 
+export var key = ""
+
 var tex_unpressed: Texture = preload("res://assets/images/button_unpressed.png")
 var tex_pressed: Texture = preload("res://assets/images/button_pressed.png")
 
@@ -9,6 +11,7 @@ onready var red_timer: Timer = $RedTimer
 onready var sprite: Sprite = $Sprite
 
 func _ready() -> void:
+	sprite.texture = tex_unpressed
 	turn_off()
 
 func turn_on() -> void:
@@ -26,6 +29,12 @@ func press() -> void:
 
 func release() -> void:
 	sprite.texture = tex_unpressed
+
+func _process(delta):
+	if Input.is_action_just_pressed(key):
+		self.press()
+	elif Input.is_action_just_released(key):
+		self.release()
 
 # Called when the timer runs out.
 func _on_RedTimer_timeout():
