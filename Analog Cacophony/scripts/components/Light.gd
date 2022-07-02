f4bb592af2e77a7b5179c57150f77dd970653607
winterbeak <winterbeak@gmail.com>
2022-07-02 09:25:45 -0400

    Light sprites

--- Analog Cacophony/scripts/components/Light.gd
+++ Analog Cacophony/scripts/components/Light.gd
@@ -1,33 +1,31 @@
 extends Node2D
 
-const ON_COLOR: Color = Color(1.0, 1.0, 0.0)
-const OFF_COLOR: Color = Color(0.4, 0.4, 0.4)
-const RED_COLOR: Color = Color(1.0, 0.0, 0.0)
-
 const RED_TIME: float = 0.8
 
-onready var color_rect: ColorRect = $ColorRect
+var tex_unpressed: Texture = preload("res://assets/images/button_unpressed.png")
+var tex_pressed: Texture = preload("res://assets/images/button_pressed.png")
+
 onready var red_timer: Timer = $RedTimer
+onready var sprite: Sprite = $Sprite
 
 func _ready() -> void:
 	turn_off()
 
 func turn_on() -> void:
-	color_rect.color = ON_COLOR
+	pass
 
 func turn_off() -> void:
-	color_rect.color = OFF_COLOR
+	pass
 
 # Turns the light red for an amount of time determined by RED_TIME.
 func turn_red() -> void:
-	color_rect.color = RED_COLOR
 	red_timer.start(RED_TIME)
 
 func press() -> void:
-	pass
+	sprite.texture = tex_pressed
 
 func release() -> void:
-	pass
+	sprite.texture = tex_unpressed
 
 # Called when the timer runs out.
 func _on_RedTimer_timeout():
