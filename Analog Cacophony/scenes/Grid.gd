9c93d1f233092b20a65d0ade72e99ba1045b875b
winterbeak <winterbeak@gmail.com>
2022-07-07 12:21:25 -0400

    Add major scale component

--- Analog Cacophony/scenes/Grid.gd
+++ Analog Cacophony/scenes/Grid.gd
@@ -5,12 +5,16 @@ var timer = 0
 func _ready():
 	$ChordComponent.position.x = Constants.LIGHT_DISTANCE_HORIZ * 4
 	$SequenceComponent.position.x = Constants.LIGHT_DISTANCE_HORIZ * 6
+	$ScaleComponent.position.y = Constants.LIGHT_DISTANCE_VERT
 
 func _process(delta):
 	if timer % 600 == 0:
 		$SequenceComponent.activate(3)
+	elif timer % 600 == 100:
+		$ScaleComponent.activate($ScaleComponent.note_time * 5)
 	elif timer % 600 == 200:
 		$QueryComponent.activate(3)
 	elif timer % 600 == 400:
 		$ChordComponent.activate(3)
+	
 	timer += 1
