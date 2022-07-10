a4979d50f80c610e77b3f99b24e82d67ba36cefd
winterbeak <winterbeak@gmail.com>
2022-07-10 00:21:20 -0400

    Add alternate component drum sounds

--- Analog Cacophony/scripts/components/AlternateComponent.gd
+++ Analog Cacophony/scripts/components/AlternateComponent.gd
@@ -2,18 +2,31 @@ extends "res://scripts/components/BaseComponent.gd"
 
 var current_light = 0
 
+onready var drum_sounds = [$Drums1, $Drums2, $Drums3, $Drums4]
+onready var activate_sound = $Activate
+
 func _ready() -> void:
 	lights = [$Light1, $Light2]
 	lights[1].position.x = Constants.LIGHT_DISTANCE_HORIZ
+	
+	activate_sound.position.x = Constants.LIGHT_DISTANCE_HORIZ
+	drum_sounds[0].position.x = Constants.LIGHT_CENTER_X
+	drum_sounds[2].position.x = Constants.LIGHT_CENTER_X
+	drum_sounds[1].position.x = Constants.LIGHT_DISTANCE_HORIZ + Constants.LIGHT_CENTER_X
+	drum_sounds[3].position.x = Constants.LIGHT_DISTANCE_HORIZ + Constants.LIGHT_CENTER_X
 
 func start() -> void:
 	lights[0].turn_on()
 	current_light = 0
+	
+	activate_sound.play()
 
 func on_key_press(key: String) -> void:
 	if key != lights[current_light % 2].key:
 		fail()
 	else:
+		drum_sounds[current_light].play()
+		
 		lights[current_light % 2].turn_off()
 		current_light += 1
 		if current_light == 4:
