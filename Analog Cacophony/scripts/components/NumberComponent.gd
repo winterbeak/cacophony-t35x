daa67d5dad9e28b0e6165cad63b785751ab58d58
winterbeak <winterbeak@gmail.com>
2022-07-10 22:37:39 -0400

    Add number component press noise

--- Analog Cacophony/scripts/components/NumberComponent.gd
+++ Analog Cacophony/scripts/components/NumberComponent.gd
@@ -5,6 +5,7 @@ var actual: int = 0
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 onready var number_sounds = [$One, $Two, $Three, $Four, $Five]
+onready var tick_sound = $Tick
 
 func _ready() -> void:
 	lights = [$Light]
@@ -12,6 +13,8 @@ func _ready() -> void:
 	for sound in number_sounds:
 		sound.position.x = Constants.LIGHT_CENTER_X
 		sound.position.y = Constants.LIGHT_CENTER_Y
+	tick_sound.position.x = Constants.LIGHT_CENTER_X
+	tick_sound.position.y = Constants.LIGHT_CENTER_Y
 
 func start() -> void:
 	lights[0].turn_on()
@@ -20,6 +23,8 @@ func start() -> void:
 	number_sounds[expected - 1].play()
 
 func on_key_press(key: String) -> void:
+	tick_sound.pitch_scale = 0.5 + actual*0.1
+	tick_sound.play()
 	actual += 1
 	if actual == expected:
 		lights[0].turn_off()
