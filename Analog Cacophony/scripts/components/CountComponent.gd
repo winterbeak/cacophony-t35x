2d96ae73c044db8b23608a12de69d64cfcd1f03e
winterbeak <winterbeak@gmail.com>
2022-07-10 23:21:39 -0400

    Add count component sounds

--- Analog Cacophony/scripts/components/CountComponent.gd
+++ Analog Cacophony/scripts/components/CountComponent.gd
@@ -8,13 +8,18 @@ var expected: int = 0
 var actual: int = 0
 
 var beep_count: int = 0
-const BEEP_TIME: float = 0.4
+const BEEP_TIME: float = 0.235
 onready var next_beep_timer: Timer = $NextBeepTimer
+onready var beep_sound = $G
+onready var press_sound = $C
+const SOUND_VOLUME = -4
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	lights = [$Light]
+	beep_sound.volume_db = SOUND_VOLUME
+	press_sound.volume_db = SOUND_VOLUME
 
 func start() -> void:
 	lights[0].turn_on()
@@ -22,14 +27,18 @@ func start() -> void:
 	actual = 0
 	beep_count = 1
 	next_beep_timer.start(BEEP_TIME)
+	beep_sound.play()
 
 func on_key_press(key: String) -> void:
+	press_sound.play()
 	actual += 1
 	if actual == expected:
 		lights[0].turn_off()
 		deactivate()
 
 func _on_NextBeepTimer_timeout():
-	beep_count += 1
 	if beep_count == expected:
 		next_beep_timer.stop()
+	else:
+		beep_sound.play()
+		beep_count += 1
