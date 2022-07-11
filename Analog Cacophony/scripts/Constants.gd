f3d5bedd3cef054e6127adcc1bdbc900f12c8a19
winterbeak <winterbeak@gmail.com>
2022-07-11 10:58:41 -0400

    Add once component record scratch sounds

--- Analog Cacophony/scripts/Constants.gd
+++ Analog Cacophony/scripts/Constants.gd
@@ -9,3 +9,4 @@ const LIGHT_FACE_HEIGHT: int = LIGHT_FACE_WIDTH
 # The sprite contains two pixels representing the holes on the left
 const LIGHT_CENTER_X: int = LIGHT_FACE_WIDTH / 2 + 2
 const LIGHT_CENTER_Y: int = LIGHT_FACE_HEIGHT / 2
+const LIGHT_CENTER: Vector2 = Vector2(LIGHT_CENTER_X, LIGHT_CENTER_Y)
--- Analog Cacophony/scripts/components/OnceComponent.gd
+++ Analog Cacophony/scripts/components/OnceComponent.gd
@@ -1,11 +1,29 @@
 extends "res://scripts/components/BaseComponent.gd"
 
+onready var activate_sound = $Activate
+onready var deactivate_sound = $Deactivate
+const ACTIVATE_VOLUME = 0
+const DEACTIVATE_VOLUME = -4
+onready var activate_sound_fader = $ActivateFader
+
 func _ready() -> void:
 	lights = [$Light]
+	
+	activate_sound.volume_db = ACTIVATE_VOLUME
+	deactivate_sound.volume_db = DEACTIVATE_VOLUME
+	activate_sound.position = Constants.LIGHT_CENTER
+	deactivate_sound.position = Constants.LIGHT_CENTER
 
 func start() -> void:
 	lights[0].turn_on()
+	
+	activate_sound.volume_db = ACTIVATE_VOLUME
+	activate_sound.play()
 
 func on_key_press(key: String) -> void:
+	activate_sound_fader.interpolate_property(activate_sound, "volume_db", ACTIVATE_VOLUME, -60, 0.05)
+	activate_sound_fader.start()
+	
 	lights[0].turn_off()
+	deactivate_sound.play()
 	deactivate()
