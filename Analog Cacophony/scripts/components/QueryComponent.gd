662186297cdeba9dc993b589e16050f3f72a7809
winterbeak <winterbeak@gmail.com>
2022-07-12 02:21:10 -0400

    Add query component beep sounds

--- Analog Cacophony/scripts/components/QueryComponent.gd
+++ Analog Cacophony/scripts/components/QueryComponent.gd
@@ -4,8 +4,24 @@ var pressed_lights: Array = []
 var correct_light: int = 0
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
+onready var wrong_query_sound = $C
+onready var right_query_sound = $Correct
+onready var activate_sound = $Activate
+const WRONG_QUERY_VOLUME: int = -10
+const RIGHT_QUERY_VOLUME: int = -10
+const ACTIVATE_VOLUME: int = -10
+
 func _ready() -> void:
 	rng.randomize()
+	
+	activate_sound.volume_db = ACTIVATE_VOLUME
+	wrong_query_sound.volume_db = WRONG_QUERY_VOLUME
+	right_query_sound.volume_db = RIGHT_QUERY_VOLUME
+	
+	activate_sound.position.x = Constants.LIGHT_DISTANCE_HORIZ*2 + Constants.LIGHT_CENTER_X
+	activate_sound.position.y = Constants.LIGHT_CENTER_Y
+	wrong_query_sound.position.y = Constants.LIGHT_CENTER_Y
+	right_query_sound.position.y = Constants.LIGHT_CENTER_Y
 	
 	# Lights from left to right
 	lights = [$Light1, $Light2, $Light3, $Light4]
@@ -19,6 +35,8 @@ func start() -> void:
 		light.turn_on()
 	
 	correct_light = rng.randi_range(0, 3)
+	
+	activate_sound.play()
 
 func press_light(light_num: int) -> void:
 	lights[light_num].turn_off()
@@ -26,8 +44,12 @@ func press_light(light_num: int) -> void:
 		fail()
 	elif light_num == correct_light:
 		deactivate()
+		right_query_sound.position.x = light_num*Constants.LIGHT_DISTANCE_HORIZ + Constants.LIGHT_CENTER_X
+		right_query_sound.play()
 	else:
 		pressed_lights[light_num] = true
+		wrong_query_sound.position.x = light_num*Constants.LIGHT_DISTANCE_HORIZ + Constants.LIGHT_CENTER_X
+		wrong_query_sound.play()
 
 func on_key_press(key: String) -> void:
 	for i in range(len(lights)):
