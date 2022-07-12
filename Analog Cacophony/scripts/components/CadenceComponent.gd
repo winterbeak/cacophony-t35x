9eca3fd9bfa23d309a90c2936e46897233a11c3e
winterbeak <winterbeak@gmail.com>
2022-07-12 03:14:30 -0400

    Make cadence component always outside-in

--- Analog Cacophony/scripts/components/CadenceComponent.gd
+++ Analog Cacophony/scripts/components/CadenceComponent.gd
@@ -1,17 +1,12 @@
 extends "res://scripts/components/BaseComponent.gd"
 
-var outside_is_first: bool = false
 var pressed: Array = []
 onready var sounds: Array = [$C, $E, $G]
 onready var activate_sound = $Activate
 const SOUND_VOLUME = -4
 const ACTIVATE_VOLUME = -1
 
-var rng: RandomNumberGenerator = RandomNumberGenerator.new()
-
 func _ready() -> void:
-	rng.randomize()
-	
 	lights = [$Light1, $Light2, $Light3]
 	lights[1].position.x = Constants.LIGHT_DISTANCE_HORIZ
 	lights[2].position.x = Constants.LIGHT_DISTANCE_HORIZ * 2
@@ -23,22 +18,14 @@ func _ready() -> void:
 func start() -> void:
 	activate_sound.play()
 	pressed = [false, false, false]
-	outside_is_first = rng.randi_range(0, 1) == 0
-	if outside_is_first:
-		lights[0].turn_on()
-		lights[2].turn_on()
-	else:
-		lights[1].turn_on()
+	lights[0].turn_on()
+	lights[2].turn_on()
 
 func on_key_press(key: String) -> void:
 	
 	for i in range(len(lights)):
 		if key == lights[i].key:
-			if outside_is_first and (not pressed[0] or not pressed[2]) and i == 1:
-				for light in lights:
-					light.turn_off()
-				fail()
-			elif not outside_is_first and (not pressed[1]) and (i == 0 or i == 2):
+			if (not pressed[0] or not pressed[2]) and i == 1:
 				for light in lights:
 					light.turn_off()
 				fail()
@@ -47,11 +34,8 @@ func on_key_press(key: String) -> void:
 				sounds[i].play()
 				pressed[i] = true
 				
-				if outside_is_first and pressed[0] and pressed[2]:
+				if pressed[0] and pressed[2]:
 					lights[1].turn_on()
-				elif not outside_is_first and pressed[1]:
-					lights[0].turn_on()
-					lights[2].turn_on()
 	
 	# Deactivate if everything is pressed
 	for press in pressed:
