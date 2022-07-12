30aa7ff184eb18d12efff5a6232512523115f025
winterbeak <winterbeak@gmail.com>
2022-07-11 22:31:30 -0400

    Add volume component guitar sounds

--- Analog Cacophony/scripts/components/VolumeComponent.gd
+++ Analog Cacophony/scripts/components/VolumeComponent.gd
@@ -6,27 +6,43 @@ const GOAL_VOLUME: int = 6
 const FAIL_VOLUME: int = 0
 var first_key_is_volume_up: bool = true
 
+onready var guitar_c = $C
+onready var guitar_chord = $Chord
+const GUITAR_VOLUME_STEP = 3
+
 func _ready() -> void:
 	lights = [$Light1, $Light2]
 	lights[1].position.x = Constants.LIGHT_DISTANCE_VERT
+	guitar_c.position.x = Constants.LIGHT_DISTANCE_HORIZ
+	guitar_chord.position.x = Constants.LIGHT_DISTANCE_HORIZ
 
 func start() -> void:
 	first_key_is_volume_up = not first_key_is_volume_up
 	volume = START_VOLUME
 	lights[0].turn_on()
 	lights[1].turn_on()
+	play_current_guitar()
 
 func increase_volume() -> void:
 	volume += 1
 	if volume == GOAL_VOLUME:
 		lights[0].turn_off()
 		lights[1].turn_off()
+		guitar_chord.play()
 		deactivate()
+	else:
+		play_current_guitar()
 
 func decrease_volume() -> void:
 	volume -= 1
 	if volume == FAIL_VOLUME:
 		fail()
+	else:
+		play_current_guitar()
+
+func play_current_guitar() -> void:
+	guitar_c.volume_db = -GUITAR_VOLUME_STEP*(GOAL_VOLUME - volume)
+	guitar_c.play()
 
 func on_key_press(key: String) -> void:
 	if key == lights[0].key:
