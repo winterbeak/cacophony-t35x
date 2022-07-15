7015850d6c95f6d41a2088f7f94f220395b57a5d
winterbeak <winterbeak@gmail.com>
2022-07-14 23:30:31 -0400

    Change count component sound

--- Analog Cacophony/scripts/components/CountComponent.gd
+++ Analog Cacophony/scripts/components/CountComponent.gd
@@ -8,11 +8,11 @@ var expected: int = 0
 var actual: int = 0
 
 var beep_count: int = 0
-const BEEP_TIME: float = 0.235
+const BEEP_TIME: float = 0.232
 onready var next_beep_timer: Timer = $NextBeepTimer
-onready var beep_sound = $G
-onready var press_sound = $C
-const SOUND_VOLUME = -8
+onready var beep_sounds = [$G, $G2]
+onready var press_sounds = [$C, $C2]
+const SOUND_VOLUME = -4
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
@@ -20,21 +20,23 @@ func _ready() -> void:
 	rng.randomize()
 	
 	lights = [$Light]
-	beep_sound.volume_db = SOUND_VOLUME
-	press_sound.volume_db = SOUND_VOLUME
-	beep_sound.position = Constants.LIGHT_CENTER
-	press_sound.position = Constants.LIGHT_CENTER
-
+	
+	for i in range(len(beep_sounds)):
+		beep_sounds[i].volume_db = SOUND_VOLUME
+		press_sounds[i].volume_db = SOUND_VOLUME
+		beep_sounds[i].position = Constants.LIGHT_CENTER
+		press_sounds[i].position = Constants.LIGHT_CENTER
+	
 func start() -> void:
 	lights[0].turn_on()
 	expected = rng.randi_range(1, 5)
 	actual = 0
 	beep_count = 1
 	next_beep_timer.start(BEEP_TIME)
-	beep_sound.play()
+	beep_sounds[0].play()
 
 func on_key_press(key: String) -> void:
-	press_sound.play()
+	press_sounds[actual % len(press_sounds)].play()
 	actual += 1
 	if actual == expected:
 		lights[0].turn_off()
@@ -44,5 +46,5 @@ func _on_NextBeepTimer_timeout():
 	if beep_count == expected:
 		next_beep_timer.stop()
 	else:
-		beep_sound.play()
 		beep_count += 1
+		beep_sounds[beep_count % len(press_sounds)].play()
