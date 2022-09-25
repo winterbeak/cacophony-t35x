c7d2d64a94427adb9d06e4bd2f4e845a37360847
winterbeak <winterbeak@gmail.com>
2022-09-25 16:38:25 -0400

    Add win flash sound

--- Analog Cacophony/scripts/Main.gd
+++ Analog Cacophony/scripts/Main.gd
@@ -4,8 +4,9 @@ onready var cacophony = $Cacophony
 onready var audio_instruction = $AudioInstruction
 
 onready var flash_timer = $FlashTimer
-var FLASH_TIME: float = 0.25
-var FLASH_INTERVAL: float = 0.5
+var FLASH_TIME: float = 0.165
+var LAST_FLASH_TIME: float = 0.75
+var FLASH_INTERVAL: float = 0.203
 var flash_count: int = 0
 var TOTAL_FLASHES: int = 3
 
@@ -16,8 +17,11 @@ var FADE_TIME: float = 4.0
 
 onready var purple_background = $PurpleBackground
 
+onready var win_sound = $WinSound
+
 func _ready() -> void:
 	purple_background.color.a = 0.0
+	win_sound.volume_db = -7
 	randomize()
 
 func _process(delta: float) -> void:
@@ -34,14 +38,18 @@ func _on_Cacophony_win():
 	cacophony.flash_grid(FLASH_TIME)
 	flash_timer.start(FLASH_INTERVAL)
 	flash_count += 1
+	win_sound.play()
 
 func _on_FlashTimer_timeout():
-	cacophony.flash_grid(FLASH_TIME)
+	
 	flash_count += 1
 	if flash_count >= 3:
+		cacophony.flash_grid(LAST_FLASH_TIME)
 		flash_count = 0
 		flash_timer.stop()
 		flash_transition_timer.start(FLASH_TRANSITION_TIME)
+	else:
+		cacophony.flash_grid(FLASH_TIME)
 
 func _on_FlashTransitionTimer_timeout():
 	if not cacophony.blind_mode:
