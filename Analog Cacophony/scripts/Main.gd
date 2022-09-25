d850d467323eb4df97c236962609e37da9f288ce
winterbeak <winterbeak@gmail.com>
2022-09-25 16:19:09 -0400

    Modify second fade in prep for credits screen

--- Analog Cacophony/scripts/Main.gd
+++ Analog Cacophony/scripts/Main.gd
@@ -11,7 +11,7 @@ var TOTAL_FLASHES: int = 3
 
 onready var flash_transition_timer = $FlashTransitionTimer
 var FLASH_TRANSITION_TIME: float = 5.0
-onready var fade_timer = $FadeTimer
+onready var blind_fade_timer = $FadeTimer
 var FADE_TIME: float = 4.0
 
 onready var purple_background = $PurpleBackground
@@ -25,8 +25,8 @@ func _process(delta: float) -> void:
 		audio_instruction.visible = false
 		cacophony.start()
 	
-	if not fade_timer.is_stopped():
-		var percentage_done = 1.0 - (fade_timer.time_left / FADE_TIME)
+	if not blind_fade_timer.is_stopped():
+		var percentage_done = 1.0 - (blind_fade_timer.time_left / FADE_TIME)
 		purple_background.color.a = percentage_done
 		cacophony.progress_bar.set_fraction(1.0 - percentage_done)
 
@@ -39,11 +39,13 @@ func _on_FlashTimer_timeout():
 	cacophony.flash_grid(FLASH_TIME)
 	flash_count += 1
 	if flash_count >= 3:
+		flash_count = 0
 		flash_timer.stop()
 		flash_transition_timer.start(FLASH_TRANSITION_TIME)
 
 func _on_FlashTransitionTimer_timeout():
-	fade_timer.start(FADE_TIME)
+	if not cacophony.blind_mode:
+		blind_fade_timer.start(FADE_TIME)
 
 func _on_FadeTimer_timeout():
 	purple_background.color.a = 1.0
